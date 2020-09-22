d530fc74bb71bc3fd44039b759878c229f896332
Carlos Alcala <carlos.aceves2888@alumnos.udg.mx>
2020-09-22 16:43:07 -0500

    capturar un numero e imprimirlo

--- act3.cpp
+++ act3.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 int main(){
-
+    int ent;
     cout<<"hola"<<endl;
-    
+    cout<<"dijite un numero: "<<endl;
+    cin>>ent;
+    cout<<"el numero es: "<<ent<<endl;
     return 0;
 }
